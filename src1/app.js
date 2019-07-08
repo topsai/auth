a7d735141a2ac4a2fc62fc624e5e75f9048214f8
topsai <hurte@foxmail.com>
2019-07-08 18:06:21 +0800

    update redux

--- src1/app.js
+++ src1/app.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import AppNavigationWithState from './navigators/AppNavigator'
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import AppNavigationWithState from './navigators/AppNavigator';
+import {
+  createReduxContainer,
+  createReactNavigationReduxMiddleware,
+  createNavigationReducer,
+} from 'react-navigation-redux-helpers';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-var wilddog = require('wilddog');
+// var wilddog = require('wilddog');
+
+const middleware = createReactNavigationReduxMiddleware(
+  state => state.nav,
+);
+const store = createStore(
+    reducers,
+    applyMiddleware(middleware),
+);
+
 
 class App extends Component {
-  // componentWillMount() {
-  //   wilddog.initializeApp({
-  //     authDomain: "wd1607116684hnxnbz.wilddog.com"
-  //   });
-  // }
-  render() {
-    return (
-      <Provider store={createStore(reducers, {}, applyMiddleware(thunk))}>
-        <AppNavigationWithState />
-      </Provider>
-    );
-  }
+    // componentWillMount() {
+    //   wilddog.initializeApp({
+    //     authDomain: "wd1607116684hnxnbz.wilddog.com"
+    //   });
+    // }
+
+
+    render() {
+        return (
+            <Provider store={store} storess={createStore(reducers, {}, applyMiddleware(thunk))}>
+                <AppNavigationWithState/>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- src1/navigators/AppNavigator.js
+++ src1/navigators/AppNavigator.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { StackNavigator, addNavigationHelpers } from 'react-navigation';
-import { AuthScreen, MenuListScreen, FoodScreen } from '../components';
+import {connect} from 'react-redux';
+import {createStackNavigator, addNavigationHelpers, createAppContainer} from 'react-navigation';
+import {AuthScreen, MenuListScreen, FoodScreen} from '../components';
+import {
+    createReduxContainer,
+    createNavigationReducer,
+    createReactNavigationReduxMiddleware,
+} from 'react-navigation-redux-helpers'
 
-export const AppNavigator = StackNavigator({
-  Auth: { screen: AuthScreen },
-  MenuList: { screen: MenuListScreen },
-  Food: { screen: FoodScreen }
+export const AppNavigator = createStackNavigator({
+    Auth: {screen: AuthScreen},
+    MenuList: {screen: MenuListScreen},
+    Food: {screen: FoodScreen}
 });
+const navReducer = createNavigationReducer(AppNavigator);
 
-const AppWithNavigationState = ({ dispatch, nav }) => (
-  <AppNavigator
-    navigation={addNavigationHelpers({ dispatch, state: nav })}
-    screenProps={{tintColor: '#007AFF'}}
-  />
-)
+
+// export const AppNavigator = createAppContainer(AppNavigator);
+
+const AppWithNavigationStates = ({dispatch, nav}) => (
+    <AppNavigator
+        navigation={addNavigationHelpers({dispatch, state: nav})}
+        screenProps={{tintColor: '#007AFF'}}
+    />
+);
+const AppWithNavigationState = createReduxContainer(AppNavigator);
 
 const mapStateToProps = (state) => ({
-  nav: state.nav
-})
+    nav: state.nav
+});
 
 export default connect(mapStateToProps)(AppWithNavigationState);
